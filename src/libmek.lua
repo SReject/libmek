0ca16fde5f4fb69c47aa76f79534812ae2c109e8
SReject <SReject@users.noreply.github.com>
2024-04-21 14:16:55 -0400

    feat: export lib files

---
+++ src/libmek.lua
@@ -0,0 +1,14 @@
+return {
+    utilities = {
+        class = require('common.class'),
+        utilities = require('common.utilities')
+    },
+    classes = {
+        Peripheral = require('classes.peripheral'),
+        Multiblock = require('classes.multiblock'),
+        Boiler = require('classes.boiler')
+    },
+    mixins = {
+        tank = require('mixins.tank')
+    }
+};
