55e9f54faf9526836832490fa3f481d5ea94faa6
SReject <SReject@users.noreply.github.com>
2024-04-22 06:54:34 -0400

    docs: code-code boiler class

--- src/classes/boiler.lua
+++ src/classes/boiler.lua
@@ -3,78 +3,156 @@ local Multiblock = require('classes.multiblock');
 
 local tank = require('mixins.tank');
 
-local new, Boiler = class.create(
+local
+    new,
 
-    -- constructor
-    function (super, self, name)
-        super(name);
-        self.__cache.boiler = {};
-    end,
+    ---@class Boiler: Multiblock
+    ---@field __super Multiblock
+    ---@field heatedCoolantTank Tank
+    ---@field waterTank Tank
+    ---@field cooledCoolantTank Tank
+    ---@field steamTank Tank
+    Boiler = class.create(
 
-    -- super class
-    Multiblock,
+        ---Constructor
+        ---@param super fun(pipheralName: string):nil
+        ---@param self Boiler
+        ---@param name string
+        function (super, self, name)
+            super(name);
+            self.__cache.boiler = {};
+        end,
 
-    -- Mixins
-    tank.factory('boiler', 'Steam', 'steam'),
-    tank.factory('boiler', 'Water', 'water'),
-    tank.factory('boiler', 'CooledCoolant', 'cooledCoolant'),
-    tank.factory('boiler', 'HeatedCoolant', 'heatedCoolant')
-);
+        -- Super class
+        Multiblock,
 
+        -- Mixins
+        tank.factory('boiler', 'Steam', 'steam'),
+        tank.factory('boiler', 'Water', 'water'),
+        tank.factory('boiler', 'CooledCoolant', 'cooledCoolant'),
+        tank.factory('boiler', 'HeatedCoolant', 'heatedCoolant')
+    );
+
+--- Clears the instance's cache
 function Boiler:clearCache()
     self.__super.clearCache(self);
     self.__cache.boiler = {};
 end
 
+---Gets the max rate at which water is converted to steam the instance's cache
+---
+---If the cache does not contain a given value it is retrieved from the
+---connecting peripheral.
+---
+---Units: millibuckets/tick
+---@param force boolean? When true the cache is forced to update
+---@return integer|nil
 function Boiler:getMaxBoilRate(force)
     if (force == true or self.__cache.boiler.maxBoilRate == nil) then
         self.__cache.boiler.maxBoilRate = self:call('getBoilCapacity');
     end
-    return self._cache.boiler.maxBoilRate;
+    return self.__cache.boiler.maxBoilRate;
 end
 
+---Gets the number of Superheaters associated with the boiler from the
+---instance's cache
+---
+---If the cache does not contain a given value it is retrieved from the
+---connecting peripheral.
+---@param force boolean? When true the cache is forced to update
+---@return integer|nil
 function Boiler:getSuperHeaters(force)
     if (force == true or self.__cache.boiler.superHeaters == nil) then
         self.__cache.boiler.superHeaters = self:call('getSuperHeaters');
     end
+    return self.__cache.boiler.superHeaters;
 end
 
+---Gets the current rate at which water is converted to steam
+---
+---Units: millibuckets/tick
+---@return integer|nil
 function Boiler:getBoilRate()
     return self:call('getBoilRate');
 end
 
+---Gets the temperature lost to the environment
+---
+---Units: kelvins/tick
+---@return integer|nil
 function Boiler:getEnvironmentalLoss()
     return self:call('getEnvironmentalLoss');
 end
 
+---Gets the max seen boil rate
+---
+---Units: millibuckets/tick
+---@return integer|nil
 function Boiler:getMaxSeenBoilRate()
     return self:call('getMaxBoilRate');
 end
 
+---Gets the current temperature of the boilers
+---
+---Units: kelvins
+---@return integer|nil
 function Boiler:getTemperature()
     return self:call('getTemperature');
 end
 
+---@alias BoilerValveMode
+---| "INPUT"
+---| "OUTPUT_STEAM"
+---| "OUTPUT_COOLANT"
+
+---Gets the current valve mode of the boiler
+---@return BoilerValveMode|nil
 function Boiler:getValveMode()
     return self:call('getMode');
 end
 
+---Sets the mode of the boiler valve that the underlaying peripheral is attached to
+---@param mode BoilerValveMode
 function Boiler:setValveMode(mode)
     return self:call('setMode', mode);
 end
 
+---Decrements the underlaying peripheral's valve mode
 function Boiler:decrementValveMode()
     return self:call('decrementValveMode');
 end
 
+---Increments the underlaying peripheral's valve mode
 function Boiler:incrementValveMode()
     return self:call('incrementValveMode');
 end
 
+
+---@class BoilerInfo: MultiblockInfo
+---@field boiler BoilerInfoEntry
+
+---@class BoilerInfoEntry
+---@field superHeaters integer|nil
+---@field maxBoilRate integer|nil
+---@field steamTank TankInfo|nil
+---@field waterTank TankInfo|nil
+---@field cooledCoolantTank TankInfo|nil
+---@field heatedCoolantTank TankInfo|nil
+
+---Retrieves static information pertaining to the boiler from the instance's
+---cache
+---
+---If the cache does not contain a given value it is retrieved from the
+---connecting peripheral.
+---@param force boolean? When true the cache is forced to update
+---@return BoilerInfo
 function Boiler:info(force)
     force = force == true
 
     local info = self.__super.info(self, force);
+
+    ---@cast info BoilerInfo
+
     if (info.multiblock.valid ~= true) then
         info.boiler = {}
     else
@@ -90,10 +168,33 @@ function Boiler:info(force)
     return info;
 end
 
+---@class BoilerStatus : MultiblockStatus
+---@field boiler BoilerStatusEntry
+
+---@class BoilerStatusEntry
+---@field boilRate integer|nil
+---@field cooledCoolantTank TankStatus|nil
+---@field environmentalLoss integer|nil
+---@field heatedCoolantTank TankStatus|nil
+---@field maxSeenBoilRate integer|nil
+---@field temperature integer|nil
+---@field steamTank TankStatus|nil
+---@field valveMode BoilerValveMode|nil
+---@field waterTank TankStatus|nil
+
+---Retrieves dynamic information pertaining to the boiler
+---
+---If the cache does not contain a given value it is retrieved from the
+---connecting peripheral.
+---@param force boolean? When true the cache is forced to update
+---@return BoilerStatus
 function Boiler:status(force)
     force = force == true;
 
     local status = self.__super.status(self);
+
+    ---@cast status BoilerStatus
+
     if (status.multiblock ~= true) then
         status.boiler = {};
     else
@@ -114,5 +215,8 @@ end
 
 return {
     class = Boiler,
+
+    ---Creates a new Boiler class instance
+    ---@type fun(peripheralName: string): Boiler
     create = new
 };
