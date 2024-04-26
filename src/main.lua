1508c6f1eae9c667eb0689ea9d8ea9c25ed349a1
SReject <SReject@users.noreply.github.com>
2024-04-26 11:55:19 -0400

    feat: add class for fission reactor port

--- src/main.lua
+++ src/main.lua
@@ -7,6 +7,7 @@ return {
         Peripheral = require('classes.peripheral'),
         Multiblock = require('classes.multiblock'),
         FissionReactor = require('classes.fission-reactor'),
+        FissionReactorPort = require('classes.fission-reactor-port'),
         InductionMatrix = require('classes.induction-matrix'),
         IndustrialTurbine = require('classes.industrial-turbine'),
         ThermoelectricBoiler = require('classes.thermoelectric-boiler'),
